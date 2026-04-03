144cca61af7f3a1f10e0344593530d89bcd5a377
zivd <ziad99kr@gmail.com>
2026-04-03 16:33:46 +0200

    add  main function

--- ex00/main.ts
+++ ex00/main.ts
@@ -1,50 +1,26 @@
-
-
-
-class Vector<k=number>{
-    data : number[];
-
-    constructor(data : number[]){
-        this.data = [...data] ;
-    }
-
-
-    size() : number{
-        return this.data.length ;
-    }
-
-    equal_vec_size(v : Vector<k>){
-        if(v.size() != this.size())
-            throw new Error("undefined size mismatch");
-    }
-
-    printv() : number[]{
-        console.log(this.data);
-        return this.data;
-    }
-    add(v : Vector<k>) : void{
-        this.equal_vec_size(v);
-        for(let i = 0 ; i < this.size() ; i++)
-                this.data[i]  +=  v.data[i];
-    }
-    sub(v : Vector<k>) : void{
-        this.equal_vec_size(v);
-        for(let i = 0 ; i < this.size() ; i++)
-                this.data[i]  -=  v.data[i];
-    }
-    scalar(scl :number){
-        for(let i = 0 ; i < this.size() ; i++)
-                this.data[i]  *=  scl;
-    }
- 
-
-}
-
+import { Vector } from "./Vector";
+import {Matrix} from "./Matrix" 
  function main(){
+
+    //// test Vector
     try{
             let u = new Vector([1 , 2 , 8]);
             let v = new Vector([2 , 4]);
             const scl = 3;
+            u.add(v);
+            u.printv();
+    }
+    catch(err){
+        if(err instanceof Error)
+                 console.log(err.message);
+    }
+
+    //// test Matrix
+
+    try{
+            let u = new Vector([1 , 2 , 8]);
+            let v = new Vector([2 , 4]);
+            //const scl = 3;
             u.add(v);
             u.printv();
     }
