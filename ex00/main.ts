dbb4c39bd651e72618d1021b58dd85f6966ad9c5
zivd <ziad99kr@gmail.com>
2026-04-07 03:51:27 +0200

    test ex01

--- ex00/main.ts
+++ ex00/main.ts
@@ -1,28 +1,25 @@
-import { Vector } from "./Vector";
-import {Matrix} from "./Matrix" 
+import { Vector } from "../class/Vector.ts";
+import {Matrix} from "../class/Matrix.ts" 
  function main(){
 
-    //// test Vector
-    try{
-            let u = new Vector([1 , 2 , 8]);
-            let v = new Vector([2 , 4]);
-            const scl = 3;
-            u.add(v);
-            u.printv();
-    }
-    catch(err){
-        if(err instanceof Error)
-                 console.log(err.message);
-    }
+ 
 
     //// test Matrix
 
     try{
-            let u = new Vector([1 , 2 , 8]);
-            let v = new Vector([2 , 4]);
-            //const scl = 3;
-            u.add(v);
-            u.printv();
+            let u = new Matrix([[4.3 , 2 ],[3 , 4]]);
+            let v = new Matrix([[2 , 4] , [4.4 ,8]]);
+            const scalar = 3
+
+            let uplusv : Matrix = u.add(v);
+            uplusv.printv();
+            ///////////
+            let usubv : Matrix = u.sub(v);
+            usubv.printv();
+            ////////////
+            let scalU : Matrix = u.scl(scalar);
+            scalU.printv();
+            
     }
     catch(err){
         if(err instanceof Error)
