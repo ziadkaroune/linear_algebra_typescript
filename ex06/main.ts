bc73ce58adfc29561f7c8971fd33658830085ec4
zivd <ziad99kr@gmail.com>
2026-04-08 03:49:10 +0200

    cross product main func

---
+++ ex06/main.ts
@@ -0,0 +1,25 @@
+import { cross_product } from "./cross_product.ts";
+import { Vector } from "../class/Vector.ts";
+
+function main(){
+    try{
+      var a = new Vector([1., 2., 3.]);
+      var b = new Vector([4., 5., 6.]);
+      const c = cross_product(a , b);
+      c.printv();
+
+    //////////////////////////////////
+    console.log("-----------------------------")
+    var x =  new Vector([4., 2., -3.]);
+    var y = new Vector([-2., -5., 16.]);
+    const z = cross_product(x , y);
+      z.printv();
+
+    }
+    catch(err){
+        if(err instanceof Error)
+            console.error(err.message);
+    }
+}
+
+main();
